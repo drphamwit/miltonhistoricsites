f64832ac35867589f6d4df68b047c3aeba5944e9
Luke Heafield <lukeheafield@MacBook-Pro.fios-router.home>
2020-07-13 13:56:29 -0400

    Added api function and started search

--- MiltonHistoricApp/src/components/tabs/Search.js
+++ MiltonHistoricApp/src/components/tabs/Search.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, TextInput } from 'react-native'
+import api from '../../utils/api'
 
-const Search = () => (
+
+const SearchBar = ({ searchCallBack }) => {
+  const [text, setText] = useState('')
+
+  return (
+    <View>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setText(text)}
+        value={text}
+        onSubmit={console.log('submitted')}
+      />
+    </View>
+  )
+}
+const Search = () => {
+
+    const [searchResult, setSearchResult] = useState('')
+
+    const searchCallBack = (queryString) => {
+      api.search(queryString)
+      .then(response => {
+        setSearchResult(response.items)
+      })
+    }
+
+    return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search</Text>
+        <SearchBar searchCallBack={searchCallBack}/>
       </View>
-)
+    )
+}
   
 
 export default Search
